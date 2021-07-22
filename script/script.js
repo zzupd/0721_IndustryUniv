315f850df119b1df2f95833b67ffc13a6dba5b66
zzupd <zzupd@naver.com>
2021-07-22 14:45:51 +0900

    0722_GNB작업완료

--- script/script.js
+++ script/script.js
@@ -1,6 +1,18 @@
 $(function(){
 
+    // GNB 시작
+    $(".mainLi a, #subMenuBG").mouseover(function(){
+        $(".subMenu").stop().slideDown(800);
+        $("div#subMenuBG").stop().slideDown(800);
+    });
+    
+    $(".mainLi a, #subMenuBG").mouseout(function(){
+        $(".subMenu").stop().slideUp(300);
+        $("div#subMenuBG").stop().slideUp(300);
+    });
 
+    
+    // GNB 끝
 
 
 
