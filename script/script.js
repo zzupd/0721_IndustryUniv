0e6b278b7be585ee27dea643370a5e4654c36ab2
zzupd <zzupd@naver.com>
2021-07-22 15:29:09 +0900

    0722_슬라이드쇼구현

--- script/script.js
+++ script/script.js
@@ -10,9 +10,25 @@ $(function(){
         $(".subMenu").stop().slideUp(300);
         $("div#subMenuBG").stop().slideUp(300);
     });
-
-    
     // GNB 끝
+
+    // 슬라이드쇼 시작
+    setInterval(fnSlide, 3000);
+
+    function fnSlide() {
+        $("#shuttleFrame").animate(
+            {"margin-left": "-1200px"},
+            2400,
+            function(){
+                $("#slide a:first-child").insertAfter("#slide a:last-child");
+                $("#shuttleFrame").css({"margin-left": "0"});
+            }
+        );
+    }
+
+
+    // 슬라이드쇼 끝
+
 
 
 
